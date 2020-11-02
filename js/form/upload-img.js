c029d77bbb0c8d1c6c58ae58762ad374e8b6bc26
Anastasiia Erokhina <erokhinaanastasiia@gmail.com>
2020-11-02 17:37:44 +0200

    Used arrow function in upload-img module

--- js/form/upload-img.js
+++ js/form/upload-img.js
@@ -6,18 +6,18 @@ const imgUploadPreview = window.pictureSize.imgUploadPreview;
 
 const imgUploadThumbnails = document.querySelectorAll(`.effects__preview`);
 
-uploadFileInput.addEventListener(`change`, function () {
+uploadFileInput.addEventListener(`change`, () => {
   const file = uploadFileInput.files[0];
   const fileType = file.type.toLowerCase();
 
-  const matches = FILE_TYPES.some(function (it) {
+  const matches = FILE_TYPES.some((it) => {
     return fileType.endsWith(it);
   });
 
   if (matches) {
     const reader = new FileReader();
 
-    reader.addEventListener(`load`, function () {
+    reader.addEventListener(`load`, () => {
       imgUploadPreview.src = reader.result;
       imgUploadThumbnails.forEach((elem) => {
         elem.style.backgroundImage = `url("${reader.result}")`;
